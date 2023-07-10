8b79a832ad7c0650c69bfa52b5cb5fb2eec4ee9d
CeoOfStoicism <nikola.lukov.2006@gmail.com>
2023-07-10 12:34:55 +0300

    Added routing

--- Client/the-realest-estate/src/app/features/features.module.ts
+++ Client/the-realest-estate/src/app/features/features.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { BuyComponent } from './buy/buy.component';
+import { RentComponent } from './rent/rent.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    BuyComponent,
+    RentComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   exports: [
     HomeComponent,
+    BuyComponent,
   ]
 })
 export class FeaturesModule { }
